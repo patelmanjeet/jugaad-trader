44ad444201e9b3780db125ec7b7986e8d2a3f037
Manjeet Patel <patel.manjeet@gmail.com>
2022-08-23 19:11:46 +0530

    fix jugaad_trader/kiteticker.py

--- jugaad_trader/kiteticker.py
+++ jugaad_trader/kiteticker.py
@@ -1,6 +1,53 @@
 from kiteconnect import KiteTicker
+import logging
+
+log = logging.getLogger(__name__)
 
 class ZerodhaKiteTicker(KiteTicker):
+    EXCHANGE_MAP = {
+        "nse": 1,
+        "nfo": 2,
+        "cds": 3,
+        "bse": 4,
+        "bfo": 5,
+        "bcd": 6,
+        "mcx": 7,
+        "mcxsx": 8,
+        "indices": 9,
+        # bsecds is replaced with it's official segment name bcd
+        # so,bsecds key will be depreciated in next version
+        "bsecds": 6,
+    }
+
+    # Default connection timeout
+    CONNECT_TIMEOUT = 30
+    # Default Reconnect max delay.
+    RECONNECT_MAX_DELAY = 60
+    # Default reconnect attempts
+    RECONNECT_MAX_TRIES = 50
+    # Default root API endpoint. It's possible to
+    # override this by passing the `root` parameter during initialisation.
+    ROOT_URI = "wss://ws.kite.trade"
+
+    # Available streaming modes.
+    MODE_FULL = "full"
+    MODE_QUOTE = "quote"
+    MODE_LTP = "ltp"
+
+    # Flag to set if its first connect
+    _is_first_connect = True
+
+    # Available actions.
+    _message_code = 11
+    _message_subscribe = "subscribe"
+    _message_unsubscribe = "unsubscribe"
+    _message_setmode = "mode"
+
+    # Minimum delay which should be set between retries. User can't set less than this
+    _minimum_reconnect_max_delay = 5
+    # Maximum number or retries user can set
+    _maximum_reconnect_max_tries = 300
+
     def __init__(self, api_key, access_token, debug=False, root=None,
                  reconnect=True, reconnect_max_tries=RECONNECT_MAX_TRIES, reconnect_max_delay=RECONNECT_MAX_DELAY,
                  connect_timeout=CONNECT_TIMEOUT):
